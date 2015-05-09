f938101870b5a0a418d041485b42d64ec7af7a03
Spiros Raptis <spirosrap@gmail.com>
2015-05-09 16:30:20 +0300

    feat: add tableview for favorites

---
+++ insta/FavoritesTableViewController.swift
@@ -0,0 +1,134 @@
+//
+//  FavoritesTableViewController.swift
+//  instaplaces
+//
+//  Created by Spiros Raptis on 09/05/2015.
+//  Copyright (c) 2015 Spiros Raptis. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesTableViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+        
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.navigationBarHidden = false
+        self.tabBarController!.tabBar.hidden = false;
+
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // MARK: - Table view data source
+    
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Potentially incomplete method implementation.
+        // Return the number of sections.
+        return 1
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        
+        
+        return 0
+    }
+    
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath) as! UITableViewCell
+        
+        // Configure the cell...
+        
+//        let instaLocation = self.instaLocations[indexPath.row]
+//        // Set the name and image
+//        cell.textLabel?.text = instaLocation.name
+//        cell.detailTextLabel?.text = "latitude:\(instaLocation.latitude) longitude:\(instaLocation.longitude)"
+//        cell.imageView?.image = UIImage(named: "magglass")
+        
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
+//        InstaClient.sharedInstance().getMediaFromLocation(instaLocations[indexPath.row], completionHandler: { (result, error) -> Void in
+//            if(error == nil){
+//                
+//                for m in result! {
+//                    (m as InstaMedia).instaLocation = self.instaLocations[indexPath.row]
+//                }
+//                CoreDataStackManager.sharedInstance().saveContext()
+//                let paController = self.storyboard!.instantiateViewControllerWithIdentifier("PhotoAlbumViewController")! as! PhotoAlbumViewController
+//                paController.title = self.instaLocations[indexPath.row].name
+//                
+//                //                paController.location = self.instaLocations[indexPath.row]
+//                
+//                dispatch_async(dispatch_get_main_queue()) {
+//                    self.navigationController!.pushViewController(paController, animated: true)
+//                }
+//            }else{
+//                println(error)
+//            }
+//            
+//        })
+        
+    }
+    
+    
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+    // Return NO if you do not want the specified item to be editable.
+    return true
+    }
+    */
+    
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+    if editingStyle == .Delete {
+    // Delete the row from the data source
+    tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+    } else if editingStyle == .Insert {
+    // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }
+    }
+    */
+    
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+    
+    }
+    */
+    
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+    // Return NO if you do not want the item to be re-orderable.
+    return true
+    }
+    */
+    
+    /*
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+    }
+    */
+}
---
+++ insta/ImageDetailViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ImageDetailViewController.swift
+//  instaplaces
+//
+//  Created by Spiros Raptis on 09/05/2015.
+//  Copyright (c) 2015 Spiros Raptis. All rights reserved.
+//
+
+import UIKit
+
+class ImageDetailViewController: UIViewController {
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    
+}
