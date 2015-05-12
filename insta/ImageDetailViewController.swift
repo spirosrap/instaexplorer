98046166aa03f23deb73ee582fd2f896479c52e6
Spiros Raptis <spirosrap@Spiross-iMac.local>
2015-05-12 15:32:42 +0300

    feat: Image Detail View Cosmetics

--- insta/ImageDetailViewController.swift
+++ insta/ImageDetailViewController.swift
@@ -8,48 +8,44 @@
 
 import UIKit
 import CoreData
-
 class ImageDetailViewController: UIViewController {
     @IBOutlet var imageView: UIImageView!
+    
+    @IBOutlet weak var bottomView: UIView!
     @IBOutlet var clickableText: UITextView!
-    
     @IBOutlet weak var profileImageView: UIImageView!
-    
     @IBOutlet weak var usernameTextView: UITextView!
-    
     @IBOutlet weak var LocationTextView: UITextView!
     
     var mediaID:String!
-    
     var userComment:String!
-    
     var attributedString = NSMutableAttributedString(string: "")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
-
         if let userComment = userComment{
-            var paragraph  = NSMutableParagraphStyle()
-            paragraph.alignment = .Justified
-            paragraph.lineSpacing = 3
-            attributedString  = NSMutableAttributedString(string: userComment, attributes: [NSForegroundColorAttributeName:UIColor.blackColor(),                NSFontAttributeName:UIFont(name: "Helvetica Neue", size: 13)!])
             
-
+            attributedString  = NSMutableAttributedString(string: userComment, attributes: [NSForegroundColorAttributeName:UIColor.blackColor(), NSFontAttributeName:UIFont(name: "HelveticaNeue", size: 14)!])
+            
             for t in tags(userComment){
-                var tagattributedString:NSAttributedString = NSAttributedString(string: t, attributes: [t:true,NSForegroundColorAttributeName:UIColor.blueColor(),NSFontAttributeName:UIFont(name: "Helvetica Neue", size: 13)!])
+                var tagattributedString:NSAttributedString = NSAttributedString(string: t, attributes: [t:true,NSForegroundColorAttributeName:UIColor(red: 0.000, green: 0.176, blue: 0.467, alpha: 1.00),NSFontAttributeName:UIFont(name: "HelveticaNeue", size: 14)!])
                 var range = NSString(string: attributedString.string).rangeOfString(t)
                 attributedString.replaceCharactersInRange(range, withAttributedString: tagattributedString)
             }
             
-            self.clickableText.attributedText = attributedString
+            //Bug that prevents to change the font (but not color) of attributed text in xcode 6: http://openradar.appspot.com/radar?id=5117089870249984 forces me to create a text view programmatically on top of the storyboard one.
+
+            var ct = UITextView(frame: self.view.frame)
+            ct.attributedText = attributedString
+            self.clickableText.addSubview(ct)
             
             var  tap = UITapGestureRecognizer(target: self, action: "clickableTouched:")
             self.clickableText.addGestureRecognizer(tap)
-            
         }
 
     }
+    
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
         
@@ -90,15 +86,37 @@ class ImageDetailViewController: UIViewController {
         if  !sectionInfo.objects.isEmpty{
             instaMedia = sectionInfo.objects[0] as! InstaMedia
             println(instaMedia.text)
+//            UIColor(red: 0.051, green: 0.494, blue: 0.839, alpha: 1.00) //Location
+//            UIColor(red: 0.000, green: 0.176, blue: 0.467, alpha: 1.00) //Username
+//            UIFont boldSystemFontOfSize:fontSize
+
+            
+            
+            var usernameAttr  = NSMutableAttributedString(string: instaMedia.username!, attributes: [NSForegroundColorAttributeName:UIColor(red: 0.000, green: 0.176, blue: 0.467, alpha: 1.00), NSFontAttributeName:UIFont(name: "HelveticaNeue-Bold", size: 17)!])
+            var range = NSString(string: instaMedia.username!).rangeOfString(instaMedia.username!)
+            usernameAttr.replaceCharactersInRange(range, withAttributedString: usernameAttr)
+            
+
             
             profileImageView.layer.cornerRadius = self.profileImageView.frame.size.width / 10
-            
             profileImageView.clipsToBounds = true
-            usernameTextView.text = "@" + instaMedia.username!
+            
+            var utv = UITextView(frame: usernameTextView.alignmentRectForFrame(usernameTextView.frame))
+            utv.attributedText = usernameAttr
+
+            
+            usernameTextView.addSubview(utv)
 
             
             if let locationName = instaMedia.instaLocation?.name {
-                LocationTextView.text = locationName
+                var locationAttr  = NSMutableAttributedString(string: locationName, attributes: [NSForegroundColorAttributeName:UIColor(red: 0.051, green: 0.494, blue: 0.839, alpha: 1.00), NSFontAttributeName:UIFont(name: "HelveticaNeue-Thin", size: 17)!])
+                var range = NSString(string: locationName).rangeOfString(locationName)
+                locationAttr.replaceCharactersInRange(range, withAttributedString: locationAttr)
+
+                
+                var ltv = UITextView(frame: usernameTextView.alignmentRectForFrame(LocationTextView.frame))
+                ltv.attributedText = locationAttr
+                LocationTextView.attributedText = locationAttr
             }
             setImage(instaMedia.imagePath!,imageView:imageView)
             setImage(instaMedia.profileImagePath!,imageView: profileImageView)
